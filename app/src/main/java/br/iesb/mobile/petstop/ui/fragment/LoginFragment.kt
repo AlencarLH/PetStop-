fc0c5bfb00607f4fe40a4f3e331ff20019fd21a0
Cláudio Laterza <claudio.laterza@hotmail.com>
2021-05-07 18:34:50 -0300

    navLogin

--- app/src/main/java/br/iesb/mobile/petstop/ui/fragment/LoginFragment.kt
+++ app/src/main/java/br/iesb/mobile/petstop/ui/fragment/LoginFragment.kt
@@ -5,54 +5,79 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import br.iesb.mobile.petstop.R
+import android.widget.Toast
+import br.iesb.mobile.petstop.databinding.FragmentLoginBinding
+import br.iesb.mobile.petstop.databinding.FragmentOnboardingBinding
+import com.google.firebase.auth.FirebaseAuth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentLoginBinding
+
+    fun irParaCadastro( v : View){
+        findNavController().navigate(R.id.action_loginFragment_to_cadastroFragment)
+    }
+
+    fun irParaForgot( v : View){
+        findNavController().navigate(R.id.action_loginFragment_to_forgotFragment)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.btnavlogin = this
+
+
+        return binding.root
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        //return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                LoginFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Bt cadastrar
+
+
+        /*
+        //Bt redefinir senha
+        bt_redefinir.setOnClickListener {
+            //terminar navegação
+        }
+
+
+
+
+        //Autentica Firebase
+        bt_login.setOnClickListener {
+            val email = et_login.text.toString()
+            val password = et_pass.text.toString()
+            val auth = FirebaseAuth.getInstance()
+
+            val taskDeLogin = auth.signInWithEmailAndPassword(email, password)
+            taskDeLogin.addOnCompleteListener { resultado ->
+                if (resultado.isSuccessful){
+                    //terminar navegação
+                }else{
+                    Toast.makeText(activity,"E-mail e/ou Senha Incorretos!", Toast.LENGTH_LONG).show()
                 }
+            }
+        }*/
     }
+
 }
